1bc867bb6c850c7ae8a430c37bc5591514610fdc
Isabel Restrepo <isabelrestre@gmail.com>
2018-09-07 11:35:59 -0400

    test

--- docker_utils.sh
+++ docker_utils.sh
@@ -28,7 +28,7 @@ if [ "$1" == "build-and-push" ]; then
     docker build -f Dockerfile --tag=${REPOSITORY}/${IMAGE_NAME}:${TAG} .
     docker push ${REPOSITORY}/${IMAGE_NAME}:${TAG}
     docker tag ${REPOSITORY}/${IMAGE_NAME}:${TAG} ${REPOSITORY}/${IMAGE_NAME}:latest
-    docker push ${REPOSITORY}/${IMAGE_NAME}:latest
+    # docker push ${REPOSITORY}/${IMAGE_NAME}:latest
     echo "Pushed ${REPOSITORY}/${IMAGE_NAME}:${TAG} and :latest"
 elif [ "$1" == "run-dev" ]; then
     docker run -p 3000:3000 --name ${IMAGE_NAME} ${REPOSITORY}/${IMAGE_NAME}
